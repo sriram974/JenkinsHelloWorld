e2ffb166f3c7b8219c3a431b233a3841c3e05ed2
sriram974 <81172887+sriram974@users.noreply.github.com>
2021-07-28 22:01:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Hello Shiva please help *****-----");
+		System.out.println("-----***** Hello Parvathi please help *****-----");
 	}
 }
